77474baa49eb71056e0ad13aa009bc529b1ac576
Stefan Luyten <stefan@Coruscant.local>
2015-08-02 17:52:36 +0200

    Updated

--- Parse.py
+++ Parse.py
@@ -10,6 +10,8 @@ for s in recordlist:
 		hour = s.attributes['startDate'].value[8:10]	
 		minute = s.attributes['startDate'].value[10:12]	
 		try:
-			print "%s,%s,%s,%s,%s,%d,%d,%d" % (year, month, date, hour, minute,   float(s.attributes['min'].value)*60, float(s.attributes['max'].value)*60, float(s.attributes['average'].value)*60)
+			#try to parse the consolidated values
+			print "%s,%s,%s,%s,%s,%d,%d,%d,%d" % (year, month, date, hour, minute,   float(s.attributes['min'].value)*60, float(s.attributes['max'].value)*60, float(s.attributes['average'].value)*60, float(s.attributes['recordCount'].value))
     		except:
-    			print "%s,%s,%s,%s,%s,%d,%d,%d" % (year, month, date, hour, minute, float(s.attributes['value'].value)*60, float(s.attributes['value'].value)*60, float(s.attributes['value'].value)*60)
+    			#try to parse the non-consolidated ones
+    			print "%s,%s,%s,%s,%s,%d,%d,%d,%d,+" % (year, month, date, hour, minute, float(s.attributes['value'].value)*60, float(s.attributes['value'].value)*60, float(s.attributes['value'].value)*60, 1)
